e3930129039a11236f2487f2f0135a6045b80e93
Jacob DeBerry <JMDeBerry99@gmail.com>
2021-06-12 14:12:10 -0400

    Initial solution for 24

--- src/main/java/oop/exercises/ex24/solution24.java
+++ src/main/java/oop/exercises/ex24/solution24.java
@@ -4,18 +4,35 @@
  */
 package oop.exercises.ex24;
 
+import java.util.Scanner;
+
 public class solution24 {
+
+    private static final Scanner in = new Scanner(System.in);
+
     public static void main(String[] args)
     {
-
-        //prompt for both input strings
-        String word1 = "";
-        String word2 = "";
+        System.out.println("Enter two strings and I'll tell you if they are anagrams:");
+        System.out.print("Enter the first string: ");
+        String word1 = in.nextLine();
+        System.out.print("Enter the second string: ");
+        String word2 = in.nextLine();
 
         AnagramChecker checker = new AnagramChecker();
-        checker.isAnagram(word1, word2);
+        boolean answer = checker.isAnagram(word1, word2);
 
-        //display if anagrams
+        System.out.println(results(answer, word1, word2));
 
+    }
+
+    private static String results(boolean answer, String word1, String word2) {
+        if(answer)
+        {
+            return "\"" + word1 + "\" and \"" + word2 + "\" are anagrams.";
+        }
+        else
+        {
+            return "\"" + word1 + "\" and \"" + word2 + "\" are not anagrams.";
+        }
     }
 }
